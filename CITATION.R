c8388cc65f9e221de2ce700d0f68938ad802bcd7
Ruben Valbuena <rv314@cam.ac.uk>
2017-02-07 17:13:32 +0000

    updating DOI

--- CITATION.R
+++ CITATION.R
@@ -6,8 +6,9 @@ bibentry(
 			person("Petteri", "Packalen")),          
 	version = "Version 1.0.0",
 	publisher = "Zenodo",
+	doi = "https://doi.org/10.5281/zenodo.269685"
 	year = "2017",          
 	url = ""
 )
 
-textVersion = " R. Valbuena, J. Heiskanen & P. Packalen (2017) Building Biocarbon and Rural Development in West Africa (BIODEV) project data & codes in R. Version 1.0.0. Zenodo."
+textVersion = " R. Valbuena, J. Heiskanen & P. Packalen (2017) Building Biocarbon and Rural Development in West Africa (BIODEV) project data & codes in R. Version 1.0.0. Zenodo. https://doi.org/10.5281/zenodo.269685"
